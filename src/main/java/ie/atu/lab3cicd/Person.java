42bbb08c5158383b584200963e11e4f317b48ad5
Antoj2000 <g00385306@atu.ie>
2024-10-17 14:37:18 +0100

    Added person class and PostMapping method

--- src/main/java/ie/atu/lab3cicd/Person.java
+++ src/main/java/ie/atu/lab3cicd/Person.java
@@ -1,4 +1,37 @@
 package ie.atu.lab3cicd;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Person {
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(max = 3, message = "Title cannot be more than 3 characters ")
+    private String title;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Employee ID invalid")
+    @Size(max = 7, message = "ID cannot be more than 7 characters")
+    private String id;
+
+    @Email(message = "Invalid email address")
+    private String email;
+
+    @Min(value = 0, message = "Age must be greater than or equal to 0")
+    private int age;
+
+    @NotBlank(message = "Position cannot be blank")
+    private String position;
+
+    @NotBlank(message = "Department cannot be blank")
+    private String department;
+
 }
