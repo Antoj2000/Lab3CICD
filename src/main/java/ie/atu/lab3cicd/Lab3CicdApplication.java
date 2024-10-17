0e152d7b3922a73ee0a979446a034e86afd68d2f
Antoj2000 <g00385306@atu.ie>
2024-10-17 13:45:44 +0100

    Created Project

---
+++ src/main/java/ie/atu/lab3cicd/Lab3CicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3CicdApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ie/atu/lab3cicd/Person.java
@@ -0,0 +1,4 @@
+package ie.atu.lab3cicd;
+
+public class Person {
+}
